825f649c064544a912d647f769a776e4de4b14ab
Daniel Teixeira <daniel.t.dt@gmail.com>
2013-08-31 12:18:21 -0300

    Adding collision

--- public/src/components/tiled_map.js
+++ public/src/components/tiled_map.js
@@ -2,9 +2,19 @@
 
 Crafty.c('TiledMap', {
   setTiledMap: function (name, tiledMap) {
-    this._tiled = tiledMap;
+    this.name = name;
 
-    this._tiled.tilesets.forEach(function (tileset) {
+    this._tiled = tiledMap;
+    this._dimensions = [this._tiled.width, this._tiled.height];
+
+    this._loadTilesets();
+    this._loadObjects();
+  },
+
+  _loadTilesets: function () {
+    var self = this;
+
+    self._tiled.tilesets.forEach(function (tileset) {
       var imageheight = (tileset.imageheight - (2 * tileset.margin));
       var imagewidth = (tileset.imagewidth - (2 * tileset.margin));
 
@@ -12,7 +22,7 @@ Crafty.c('TiledMap', {
       var widthInTiles = Math.floor(imagewidth / tileset.tilewidth);
 
       var map = {};
-      map['Map' + name] = [0, 0, widthInTiles, heightInTiles];
+      map['Map' + self.name] = [0, 0, widthInTiles, heightInTiles];
 
       Crafty.sprite(
         tileset.tilewidth,
@@ -23,5 +33,27 @@ Crafty.c('TiledMap', {
         tileset.margin
       );
     });
-  }
+  },
+
+  _loadObjects: function () {
+    var self = this;
+
+    self._tiled.layers.filter(function (layer) {
+      return layer.type === 'objectgroup';
+    }).forEach(function (layer, layerPosition) {
+      if (layer.name === 'collision') {
+        layer.objects.forEach(function (object) {
+          var objectType = (object.type || 'block');
+          Crafty.e('2D, Collision, ' + objectType)
+          .attr({
+            x: object.x,
+            y: object.y,
+            w: object.width,
+            h: object.height,
+            z: layerPosition
+          });
+        });
+      }
+    });
+  },
 });
