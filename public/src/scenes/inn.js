8b856a60a35f57b6eea583b6b78bec789f919dec
Tuane Quintella <q.tuane@gmail.com>
2013-09-01 12:52:59 -0300

    uhul

--- public/src/scenes/inn.js
+++ public/src/scenes/inn.js
@@ -6,6 +6,11 @@
       url: '/src/levels/inn.json'
     }).done(function (tiled) {
       Crafty.e('TiledMap').setTiledMap(tiled);
+      var player = Crafty('PlayerCharacter');
+      var map = new MapHandler(player.x, player.y, Config.currentLevel);
+      player.bind('Move', function () {
+        map.changeLoc(this.x, this.y);
+      });
       
       Crafty.audio.stop();
       Crafty.audio.play('inn_song', -1);
--- public/src/scenes/tubarerna.js
+++ public/src/scenes/tubarerna.js
@@ -6,7 +6,12 @@
       url: '/src/levels/tubarerna.json'
     }).done(function (tiled) {
       Crafty.e('TiledMap').setTiledMap(tiled);
-
+      var player = Crafty('PlayerCharacter');
+      var map = new MapHandler(player.x, player.y, Config.currentLevel);
+      player.bind('Move', function () {
+        map.changeLoc(this.x, this.y);
+      });
+      
       Crafty.audio.stop();
       Crafty.audio.play('tubarerna_song', -1);
 
